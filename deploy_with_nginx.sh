1eaeac40ff8802ecb4c60ec202a5fb89689049d3
Sadiqul Islam <104644991+Sadiq-code-nest@users.noreply.github.com>
2026-04-22 23:01:07 +0600

    Update deploy_with_nginx.sh

--- deploy_with_nginx.sh
+++ deploy_with_nginx.sh
@@ -6,7 +6,7 @@
 set -e
 
 # ── CONFIG ───────────────────────────────────────────────────
-REPO_URL="https://github.com/your-username/devops-academy.git"
+REPO_URL="https://github.com/Sadiq-code-nest/devops-academy-MERN.git"
 APP_DIR="/var/www/devops-academy"
 WEB_ROOT="/var/www/devops-academy-ui"
 BACKEND_PORT=5000
